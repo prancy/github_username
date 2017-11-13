a5606b8a753aa6edda8031df54e44c6d240dec69
nancy nguyen <prancynguyen@gmail.com>
2017-11-13 12:04:37 -0800

    Fixed ability to search for Github Usernames

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
+const rootURL = 'https://api.github.com/';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {userData: null});
 });
 router.post('/', function(req, res, next) {
-  console.log( `username: ${req.body.username}`);
-  res.render('index');
+  var options = {
+    url: `${rootURL}users/${req.body.username}`,
+    headers: {
+      'User-Agent': 'prancy',
+      'Authorization': `token ${process.env.GITHUB_TOKEN}`
+    }
+  };
+  request (options, function(err, response, body) {
+    var userData = JSON.parse(body);
+    options.url = userData.repos_url;
+    request(options, function(err, response, body) {
+      userData.repos = JSON.parse(body);
+      console.log(userData.repos[0]);
+      res.render('index', { userData });
+    });
+  });
 });
 
 module.exports = router;
